8566c4ed617d62297d1f918a3ae8f2b319da4238
shinichi69-app <popsteptep555@gmail.com>
2026-09-04 22:31:06 +0700

    Delete file.js

--- file.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', e => {
-    const data = e.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: 'https://via.placeholder.com/128' // ใส่ URL ไอคอนแอปของคุณ
-    });
-});
